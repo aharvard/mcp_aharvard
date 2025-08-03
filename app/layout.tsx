41d2f6c603e347101421110fba2fbc426f5346a6
Andrew Harvard <andrew.t.harvard@gmail.com>
2025-08-03 10:18:51 -0400

    Remove unused ClientOnly component import from layout.tsx to streamline code and improve maintainability.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "../components/Navigation";
 import { ToastContainer } from "../components/ToastContainer";
-import ClientOnly from "../components/ClientOnly";
 
 const inter = Inter({ subsets: ["latin"] });
 
