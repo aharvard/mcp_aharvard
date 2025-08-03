87fef51ea5a187d332e2d3f53e813f8084089aec
Andrew Harvard <andrew.t.harvard@gmail.com>
2025-08-03 10:23:06 -0400

    Remove unused import of createPortal in Toast component to streamline code.

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { createPortal } from "react-dom";
 
 export type ToastType = "success" | "error" | "warning" | "info";
 
